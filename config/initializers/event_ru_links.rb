bd41c554200244549e6048137e5f128221b625e9
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2013-01-29 11:19:51 +0300

    setup social links on left

--- config/initializers/event_ru_links.rb
+++ config/initializers/event_ru_links.rb
@@ -2,5 +2,8 @@ module EventRu
   module Links
     Facebook = 'https://www.facebook.com/WelcomeToEvent.ru'
     Vkontakte = '#'
+    Twitter = '#'
+    MailRu = '#'
+    GooglePlus = '#'
   end
 end
