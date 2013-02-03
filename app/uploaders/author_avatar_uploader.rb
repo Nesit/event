c06551f6c0a9f0d3a8f1e0eedc53e7cc90799bbb
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2013-02-03 21:01:31 +0300

    makeup fixes

--- app/uploaders/author_avatar_uploader.rb
+++ app/uploaders/author_avatar_uploader.rb
@@ -2,10 +2,9 @@
 
 class AuthorAvatarUploader < BasicImageUploader
   process convert: :png
-  process resize_to_fill: [96, 96]
 
   version :thumb do
-    process resize_to_fill: [16, 16]
+    process resize_to_fill: [32, 32]
     process convert: :png
   end
 end
