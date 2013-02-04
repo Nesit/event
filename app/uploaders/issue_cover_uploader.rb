fc16a07d152bf1c935fa554f5b8c693531863750
Maxim Kremenev <ezotrank@gmail.com>
2013-02-04 15:49:42 +0300

    #EVENT-162 fix
    - Change index form on Admin Issue for reduce width
    - Change thumb image in IssueCoverUploated to '100x100'

--- app/uploaders/issue_cover_uploader.rb
+++ app/uploaders/issue_cover_uploader.rb
@@ -4,6 +4,11 @@ class IssueCoverUploader < BasicImageUploader
   process convert: :png
 
   version :thumb do
+    process resize_to_fill: [100, 100]
+    process convert: :png
+  end
+
+  version :thumb_ext do
     process resize_to_fill: [200, 283]
     process convert: :png
   end
