923fa9506a071e419cc6eda7f8bb762ccb7553c4
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2013-02-08 12:25:13 +0300

    fixes EVENT-178

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class HomeController < ApplicationController
   authorize_resource class: false
 
@@ -5,6 +7,8 @@ class HomeController < ApplicationController
     @carousel_articles = Article.without_tv.newer_published.first(5)
     @afisha_articles = EventArticle.newer_targeted.first(6)
     @articles = Article.without_tv.newer_published.page(params[:page]).per(10)
+    @seo_tags ||= {}
+    @seo_tags[:title] = "Event.ru - информационный портал event-индустрии"
     render template: 'articles/index'
   end
 
