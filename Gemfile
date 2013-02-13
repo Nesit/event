43178f27aca087661f77bfe8e41bec2aade0f829
Maxim Kremenev <ezotrank@gmail.com>
2013-02-13 10:34:42 +0300

    try add capistrano deploy

--- Gemfile
+++ Gemfile
@@ -55,6 +55,10 @@ group :development do
   gem 'sextant'
   gem 'quiet_assets'
   gem 'letter_opener'
+  gem 'capistrano'
+  gem 'capistrano-ext'
+  gem 'capistrano-unicorn'
+  gem 'rvm-capistrano'
 end
 
 group :test, :development do
---
+++ config/deploy/staging.rb
@@ -0,0 +1,22 @@
+set :stage, 'staging'
+
+role :web, "stage.event.elzar.srv.balticit.ru"
+role :app, "stage.event.elzar.srv.balticit.ru"
+role :db,  "stage.event.elzar.srv.balticit.ru", :primary => true
+
+set :user, "event"
+set :deploy_to, "/var/www/event_staging"
+
+set :rails_env, "staging"
+set :branch, "stage"
+set :unicorn_env, 'staging'
+set :keep_releases, 5
+set :app_env, 'staging'
+
+set :rake, "#{File.join shared_path, 'scripts/rvm_wrapper.sh'} bundle exec rake"
+
+before 'db:drop', 'unicorn:stop'
+before 'db:create', 'db:drop'
+after 'db:seed', 'db:load_sample'
+
+before 'unicorn:restart', 'deploy:symlink_robots'
