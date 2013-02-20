dca4b65a569e7c3db8f56cda1696d4c036c62659
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2013-02-20 14:59:38 +0300

    add completeness hotfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -186,6 +186,7 @@ class User < ActiveRecord::Base
   end
 
   def select_state
+    self.state = 'complete'
     complete = true
     FIELDS_FOR_COMPLETE.each do |sym|
       if send(sym).blank?
@@ -193,6 +194,7 @@ class User < ActiveRecord::Base
         break
       end
     end
+    self.state = 'need_info' unless complete
   end
 
   # use separate validator instead of uniqueness: true
