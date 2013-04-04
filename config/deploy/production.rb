73a2baa6198004e7cce8db335323dcb2cc11646c
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2013-04-04 23:04:36 +0300

    add same deploy rules for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,3 +14,15 @@ set :keep_releases, 10
 set :app_env, 'production'
 
 set :rake, "#{File.join shared_path, 'scripts/rvm_wrapper.sh'} #{rake}"
+
+after 'deploy:symlink_shared', 'deploy:symlink_sphinx_config'
+
+before 'sphinx:stop', 'sphinx:configure'
+before 'db:drop', 'unicorn:stop', 'sphinx:stop'
+before 'db:create', 'db:drop'
+after 'db:seed', 'db:load_sample'
+
+before 'unicorn:restart', 'sphinx:rebuild'
+before 'unicorn:restart', 'deploy:symlink_robots'
+after 'unicorn:restart', 'weather:update'
+after 'unicorn:restart', 'whenever:update'
